5d71af677be1d660f602e0d5f16544e5f0ff9045
MadisonViaLux <madison.s.mckown@gmail.com>
2019-07-26 09:31:26 -0700

    set up v2

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
+
+
+
 import "./index.scss";
 
 
 class App extends Component {
+
+
     render() {
       return (
+
         <div className="App">
           <h1>SMURFS! 2.0 W/ Redux</h1>
+
           <div>Welcome to your state management version of Smurfs!</div>
+
           <div>Start inside of your `src/index.js` file!</div>
+
           <div>Have fun!</div>
+          
         </div>
+
       );
     }
   }
