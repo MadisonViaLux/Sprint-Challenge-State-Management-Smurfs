6639afd248e90467b941eb375ce8974bab5c008d
MadisonViaLux <madison.s.mckown@gmail.com>
2019-07-26 10:07:01 -0700

    basic reducer built out

--- smurfs/src/components/reducers/index.js
+++ smurfs/src/components/reducers/index.js
@@ -1,1 +1,47 @@
 import {GRAB_SMURF_START, GRAB_SMURF_SUCCESS, GRAB_SMURF_FAILURE} from '../actions/index'
+
+
+
+const initialState = {
+    error: "",
+    isLoading: false,
+    name: "",
+    age: "",
+    height: ""
+  };
+
+
+
+export const reducer = (state = initialState, action) => {
+
+    console.log('THIS IS THE REDUCER---', action)
+
+    switch(action.type){
+
+        case GRAB_SMURF_START:
+            return {
+                ...state,
+                isLoading: true
+            };
+
+            case GRAB_SMURF_SUCCESS:
+                return {
+                    ...state,
+                    error: '',
+                    isLoading: false,
+                    name: action.payload.name,
+                    age: action.payload.age,
+                    height: action.payload.height
+                }
+
+            case GRAB_SMURF_FAILURE:
+                return {
+                    ...state,
+                    error: 'you done messed up your code A-A-ron',
+                    isLoading: false, 
+                }
+
+            default: return state;
+
+    }
+}
