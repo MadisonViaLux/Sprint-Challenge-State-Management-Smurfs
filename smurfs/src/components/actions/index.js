cf702e1d9def5b3ac828d545a3982a7dff458d7f
MadisonViaLux <madison.s.mckown@gmail.com>
2019-07-26 09:38:17 -0700

    building out actions for redux

--- smurfs/src/components/actions/index.js
+++ smurfs/src/components/actions/index.js
@@ -1,1 +1,18 @@
 import axios from 'axios'
+
+export const GRAB_SMURF_START = "GRAB_SMURF_START"
+
+export const GRAB_SMURF_SUCCESS = "GRAB_SMURF_SUCCESS"
+
+export const GRAB_SMURF_FAILURE = "GRAB_SMURF_FAILURE"
+
+
+
+export const getDatSmurf = () => dispatch => {
+    dispatch({GRAB_SMURF_START});
+
+    return axios
+        .get()
+        .then()
+        .catch()
+}
